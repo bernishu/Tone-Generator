daf47349030f17ec4314b2e2bd874d6dd70eed04
Yacob <bernishu@gmail.com>
2026-08-30 21:36:00 -0400

    Delete build.bat

--- build.bat
+++
@@ -1,3 +0,0 @@
-@rem Build on Windows with MinGW-w64 (MSYS2: pacman -S mingw-w64-x86_64-gcc)
-windres app.rc -O coff -o app.res
-gcc -O2 -Wall -Wextra -mwindows DualToneGenerator.c app.res -o DualToneGenerator.exe -lwinmm -lcomctl32 -lcomdlg32 -lgdi32 -static -s
